ec27efe47a0f6ef7fa57c22c40c01c140d3bbdf9
zhongjie3 <zhongjie3@staff.weibo.com>
2017-06-23 16:02:17 +0800

    aaaaaaaaaaaaaaaa

--- URLSessionTest/URLSessionManager/JZJDownloadManager.m
+++ URLSessionTest/URLSessionManager/JZJDownloadManager.m
@@ -146,7 +146,7 @@ static JZJDownloadManager *instance = nil;
     self.lock = [[NSLock alloc] init];
     self.lock.name = JZJDownloadManagerLockName;
     
-    NSLog(@"222222222222222222");
+    NSLog(@"aaaaaaaaaaaaaaaaaa");
     
     [self _initSession];
 }
